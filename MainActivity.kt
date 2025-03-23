9c2e3c19aed94821b4e935b37fa557112c03015f
curixz <gabrielunivap123@gmail.com>
2025-03-23 10:04:25 -0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.calculadoraimc
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,34 +9,26 @@ import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var edtPeso: EditText
-    private lateinit var edtAltura: EditText
-    private lateinit var btnCalcular: Button
-    private lateinit var txtResultado: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edtPeso = findViewById(R.id.edtPeso)
-        edtAltura = findViewById(R.id.edtAltura)
-        btnCalcular = findViewById(R.id.btnCalcular)
-        txtResultado = findViewById(R.id.txtResultado)
+        // Correção: tipagem explícita
+        val edtPeso = findViewById<EditText>(R.id.edtPeso)
+        val edtAltura = findViewById<EditText>(R.id.edtAltura)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
 
         btnCalcular.setOnClickListener {
-            calcularIMC()
-        }
-    }
+            val peso = edtPeso.text.toString().toFloatOrNull()
+            val altura = edtAltura.text.toString().toFloatOrNull()
 
-    private fun calcularIMC() {
-        val peso = edtPeso.text.toString().toFloatOrNull()
-        val altura = edtAltura.text.toString().toFloatOrNull()
-
-        if (peso != null && altura != null && altura > 0) {
-            val imc = peso / (altura * altura)
-            txtResultado.text = "IMC: %.2f\n$imc".format(imc)
-        } else {
-            txtResultado.text = "Por favor, insira valores válidos!"
+            if (peso != null && altura != null && altura > 0) {
+                val imc = peso / (altura * altura)
+                txtResultado.text = "IMC: %.2f\n$imc".format(imc)
+            } else {
+                txtResultado.text = "Por favor, insira valores válidos!"
+            }
         }
     }
 }
